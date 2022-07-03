19c1892d1d4ef5d87c98d6f111c96bc84e3394cc
William Wang <williamwyj1@gmail.com>
2022-07-03 13:54:33 -0700

    home page video

---
+++ react-frontend/src/components/Homepage.tsx
@@ -0,0 +1,10 @@
+export default function Homepage() {
+  <div>
+    <video
+      src='../../public/video/homepage/homepage_video.mp4'
+      muted={true}
+      loop={true}
+      autoPlay={true}
+    ></video>
+  </div>;
+}
