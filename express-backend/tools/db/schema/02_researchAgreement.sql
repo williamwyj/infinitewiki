0d3c53eaa44f4b58f1ddbb22d365de7371aa3682
William Wang <williamwyj1@gmail.com>
2022-08-22 15:47:06 -0700

    finish research agreement data input

--- express-backend/tools/db/schema/02_researchAgreement.sql
+++ express-backend/tools/db/schema/02_researchAgreement.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS researchAgreement;
 CREATE TABLE IF NOT EXISTS researchAgreement (
   researchId INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  shipName varchar(20) NOT NULL,
+  shipName varchar(225) NOT NULL,
   weightData smallint NOT NULL,
-  industryFilter varchar(20) NULL,
-  strategicFilter varchar(20) NULL,
-  tacticalFilter varchar(20) NULL
+  industryFilter varchar(225) NULL,
+  strategicFilter varchar(225) NULL,
+  tacticalFilter varchar(225) NULL
 )
